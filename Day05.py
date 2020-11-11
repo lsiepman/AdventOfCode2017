91e54408d572e0bb6e2e2b28a8b7fb200e9277a2
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-11-11 23:50:17 +0100

    Added day 5

---
+++ Day05.py
@@ -0,0 +1,70 @@
+# IMPORTS
+
+# DATA
+data = []
+with open("Data - Day05.txt") as file:
+    for line in file:
+        data.append(int(line))
+
+part2_data = data.copy()
+# GOAL 1
+"""
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+Start at the first instruction ***REMOVED*** 
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+"""
+
+# ANSWER 1
+def part_1(data):
+    final = len(data)
+    
+    count = 0
+    idx = 0
+    while idx < final:
+        val = data[idx]
+        data[idx] += 1
+        idx = idx + val
+        count += 1
+    
+    print(f"Answer 5a: {count}")
+
+part_1(data)
+
+# GOAL 2
+"""
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+"""
+
+# ANSWER 2
+def part_2(data):
+    final = len(data)
+    
+    count = 0
+    idx = 0
+    while idx < final:
+        val = data[idx]
+        if val >= 3:
+            data[idx] -= 1
+        else:
+            data[idx] += 1
+        
+        idx = idx + val
+        count += 1
+    
+    print(f"Answer 5b: {count}")
+
+part_2(part2_data)
