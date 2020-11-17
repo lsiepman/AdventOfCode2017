52495398244e5972081b0ab1b842eb83a9edc9a4
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-11-17 22:49:44 +0100

    Added day 9
    I really liked this one

---
+++ Day09.py
@@ -0,0 +1,61 @@
+# IMPORTS
+import re
+
+# DATA
+file = open("Data - Day09.txt")
+data = file.read().strip()
+# GOAL 1
+"""***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+"""
+
+# ANSWER 1
+without_cancel = re.sub(r"!.{1}", "", data)
+without_garbage = re.sub(r"<[^>]*>", "", without_cancel)
+
+def get_score(text):
+    score = 0
+
+    current_level = 0
+    for i in text:
+        if i == "{":
+            current_level += 1
+        elif i == "}":
+            score += current_level 
+            current_level -= 1
+        else:
+            pass
+    
+    return score
+
+print(f"Answer 9a: {get_score(without_garbage)}")
+
+# GOAL 2
+"""
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+"""
+
+garbage = re.findall(r"<([^>]*)>", without_cancel)
+garbage_text = "".join(garbage)
+
+print(f"Answer 9b: {len(garbage_text)}")
