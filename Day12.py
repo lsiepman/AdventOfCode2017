9e2f788fbe5142f4c8130c2e59e13fd1e4fc0b6e
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-11-28 00:09:48 +0100

    Worked on day12 part 2
    It still needs some debugging for data['333']

--- Day12.py
+++ Day12.py
@@ -6,7 +6,7 @@ data = {}
 with open("Data - Day12.txt") as file:
     for line in file:
         key = re.search("^[0-9]+", line).group()
-        connections = re.search(">([0-9,\s]+)", line).group(1).strip()
+        connections = re.search(r">([0-9,\s]+)", line).group(1).strip()
         data[key] = connections.split(", ")
 
 # GOAL 1
@@ -54,3 +54,32 @@ print(f"Answer part 1: {find_connections(data)}")
 ***REMOVED***
 Now, they would like you to determine the total number of groups.
 """
+def find_members(data, group="0"):
+    old = set(group)
+    new = set()
+
+    while True: 
+        for i in old:
+            new.update(data[i])
+
+        if len(old) == len(new):
+            return new
+        else:
+            old = old | new
+
+def find_groups(data):
+    all_progs = set(data.keys())
+    grouped_progs = set()
+    num_groups = 0
+    
+    while len(grouped_progs) < len(all_progs):
+        not_in_common = all_progs - grouped_progs
+        grouped_progs.update(find_members(data, group=list(not_in_common)[0]))
+        num_groups += 1
+
+    return num_groups
+
+print(f"There are {find_groups(data)} groups in the data")
+
+    
+
