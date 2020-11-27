b8b7e6b11565c3d640fa9dcfbb7bf2c4fb0892a9
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-11-27 23:48:56 +0100

    Finished the first part of day 12

---
+++ Day12.py
@@ -0,0 +1,56 @@
+# IMPORTS
+import re
+
+# DATA
+data = {}
+with open("Data - Day12.txt") as file:
+    for line in file:
+        key = re.search("^[0-9]+", line).group()
+        connections = re.search(">([0-9,\s]+)", line).group(1).strip()
+        data[key] = connections.split(", ")
+
+# GOAL 1
+"""***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+How many programs are in the group that contains program ID 0?
+"""
+
+# ANSWER 1
+def find_connections(data, group="0"):
+    old = set(group)
+    new = set()
+
+    while True: 
+        for i in old:
+            new.update(data[i])
+
+        if len(old) == len(new):
+            return (len(new))
+        else:
+            old = old | new
+
+print(f"Answer part 1: {find_connections(data)}")
+
+# GOAL 2
+"""
+***REMOVED***
+***REMOVED***
+***REMOVED***
+Now, they would like you to determine the total number of groups.
+"""
